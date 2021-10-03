5915a0dc23994b14f7a8fda569c641490392fa11
SoubirSaian <soubir2018@gmail.com>
2021-10-04 01:13:48 +0530

    initial-commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import { BrowserRouter as Router,Switch,Route } from 'react-router-dom';
+import './App.css';
+import About from './components/About/About';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import NotFound from './components/NotFound/NotFound';
+import Service from './components/Service/Service';
+
+function App() {
+  return (
+    <div>
+      <h1>starting page</h1>
+      <Router>
+        <Header></Header>
+          <Switch>
+            <Route exact path="/">
+                <Home></Home>
+            </Route>
+            <Route path="/home">
+                <Home></Home>
+            </Route>
+            <Route path="/service">
+                <Service></Service>
+            </Route>
+            <Route path="/about">
+                <About></About>
+            </Route>
+            <Route path="*">
+                <NotFound></NotFound>
+            </Route>
+          </Switch>
+          <Footer></Footer>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
